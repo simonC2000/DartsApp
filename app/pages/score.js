a92a4afd8d1e7ea36008406bc50f74e049028e8a
Simon Coppens <simoncoppens@MacBook-Pro-van-Simon-2.local>
2022-01-18 12:33:11 +0100

    code on function

--- app/pages/score.js
+++ app/pages/score.js
@@ -1,6 +1,8 @@
 import document from 'document';
 import { switchPage } from '../navigation';
 
+let $scoreNumber = 501;
+let $scoreNumberText = 'CONFIRM         ' + 'SCORE: ' + $scoreNumber;
 
 function doSomething() {
   console.log('hallo index');
@@ -13,41 +15,16 @@ export function destroy() {
 }
 
 export function init() {
-  let $scoreNumber = 501;
-  let $tumblerButton = document.getElementById('tumbler-score');
+  let $tumblerButton = document.getElementById('score');
   let $tumblerScore = document.getElementById('tumbler-score');
   let $number = document.getElementById('score');
-  $number.text = $scoreNumber;
+  $number.text = $scoreNumberText;
   $tumblerButton.onclick = () => {
   console.log(parseInt($tumblerScore.value));
   let $result = $scoreNumber - parseInt($tumblerScore.value);
   console.log($result);
-  $number.text = $result;
+  $number.text = 'CONFIRM         ' + 'SCORE: ' + $result;
   }
 };
 
 doSomething();
-
-
-/*export function init() {
-  console.log('init index page');
-  let $startScore = 501;
-  let $scoreNumber = document.createElement($startScore);
-  let $scoreNumberTwo = "";
-  let $scoreText = document.getElementById("score");
-  $scoreText.text = $scoreNumber;
-  $buttonTumbler = document.getElementById('buttonTumbler');
-
-  let $tumblerScore = document.getElementById("tumbler-score");
-  let $tumblerButtonThree = document.getElementById("score-item3");
-
-  $tumblerButtonThree.onclick = () => {
-    console.log("click");
-    $scoreNumberTwo = $scoreNumber - 3;
-    console.log($scoreNumberTwo)
-    $scoreText.text = $scoreNumberTwo; 
-  }; 
-
-
-  doSomething();
-} */

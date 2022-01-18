c0a1dde376f013a533b071cfc8eca090aaaa4c1c
Simon Coppens <simoncoppens@MacBook-Pro-van-Simon-2.local>
2022-01-18 20:35:57 +0100

    DONE

--- app/pages/score.js
+++ app/pages/score.js
@@ -37,31 +37,3 @@ export function init() {
 }
 };
 doSomething();
-
-/*
-
-export function init() {
-  let $tumblerButton = document.getElementById('score');
-  let $tumblerScore = document.getElementById('tumbler-score');
-  let $previousScore = document.getElementById('score');
-  $previousScore.text = $scoreNumber;
-  $tumblerButton.onclick = () => {
-    if($previousScore.text >= 1) {
-      console.log($previousScore.text);
-      let $scoreRemaining = $previousScore.text - parseInt($tumblerScore.value);
-      console.log($tumblerScore.value);
-      console.log($scoreRemaining);
-      $previousScore.text = $scoreRemaining;
-    }
-    else if ($previousScore.text == 0) {
-      console.log('win');
-      switchPage('win', true);
-    }  
-    else {
-      console.log('error');
-    }  
-}
-};
-doSomething();
-
-*/
